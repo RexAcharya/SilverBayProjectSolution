d666a4df222295a62b2ae9700dc7374783fd4963
Rex Acharya <rexacharya1@outlook.com>
2022-05-27 01:03:43 -0500

    added first front end capability

--- Startup.cs
+++ Startup.cs
@@ -67,11 +67,18 @@ namespace SilvarBayAPI
                 };
             });
 
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ApplicationDbContext db)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
